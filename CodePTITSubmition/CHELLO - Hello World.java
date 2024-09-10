3551a5cda1f039701b1caff0b09034ab5ea4d9c4
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-09-10 11:03:27 +0700

    CHELLO - Hello World

--- CodePTITSubmition/CHELLO - Hello World.java
+++ CodePTITSubmition/CHELLO - Hello World.java
@@ -1,5 +1,5 @@
-public class CHELLO {
-    public static void main(String[] args) {
-        System.out.println("Hello PTIT.");
-    }
+public class CHELLO - Hello World{
+   public static void main(String[] args) {
+       System.out.println("Hello PTIT.");
+   }
 }
