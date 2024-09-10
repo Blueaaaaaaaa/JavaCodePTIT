8b6e55f73feb747b0857964c3142a5c97c402c4e
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-09-10 23:02:44 +0700

    J01003

--- CodePTITSubmition/J01003.java
+++ CodePTITSubmition/J01003.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+public import java.util.Scanner;
 
-public class HaiLam {
+public class J01003 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
@@ -17,4 +17,7 @@ public class HaiLam {
 
         sc.close();
     }
+}
+ {
+   
 }
