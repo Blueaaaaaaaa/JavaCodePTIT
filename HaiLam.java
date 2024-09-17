7f802680d4471134224b1d1f7f457482d935d526
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-09-18 00:48:37 +0700

    Update Java files

--- HaiLam.java
+++ HaiLam.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 
 public class HaiLam {
-    private static int isPrime(int n) {
-        if (n < 2) {
-            return 0;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return 0; 
-            }
-        }
-        return 1;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t > 0) {
+        int test = sc.nextInt();
+        while (test-- > 0) {
             int n = sc.nextInt();
-            if (isPrime(n) == 0) {
-                System.out.println("NO");
-            } else {
-                System.out.println("YES");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
             }
+            boolean isPalindrome = true;
+            for (int i = 0; i < n / 2; i++) {
+                if (a[i] != a[n - i - 1]) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            System.out.println(isPalindrome ? "YES" : "NO");
         }
+        sc.close();
     }
+}
