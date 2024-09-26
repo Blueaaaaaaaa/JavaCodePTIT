5f8452862276b07a61edb30e9d616c8dcc955e59
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-09-27 01:10:54 +0700

    Add J01004

--- HaiLam.java
+++ HaiLam.java
@@ -3,28 +3,28 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class HaiLam {
-    public static int tong(int n) {
-        int sum = 0;
-        while (n > 0) {
-            sum += n % 10;
-            n /= 10;
+    public static int primes(int n) {
+        if (n < 2){
+            return 0;
         }
-        return sum;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        Integer[] arr = new Integer[a];
-        for (int i = 0; i < a; i++) {
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr, new Comparator<Integer>() {
-            public int compare(Integer a, Integer b) {
-                return tong(a) - tong(b);
+        int testcase = sc.nextInt();
+        while (testcase-- > 0) {
+            int n = sc.nextInt();
+            if (primes(n) == 1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
             }
-        });
-        for (int x : arr) {
-            System.out.println(x);
         }
     }
 }
