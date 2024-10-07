93777186a6e96722a1fc691fd5140d4ccb58306b
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-10-07 14:07:01 +0700

    Cập nhật mã nguồn

--- CodePTITSubmition/J02013.java
+++ CodePTITSubmition/J02013.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Code {
+public class J02013 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
