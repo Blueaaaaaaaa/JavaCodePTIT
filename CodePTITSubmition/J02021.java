5965f9ef8609c2ad75e801d931eb81f86afbb1f8
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-10-07 14:20:16 +0700

    Cập nhật mã nguồn

--- CodePTITSubmition/J02021.java
+++ CodePTITSubmition/J02021.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Code {
+public class J02021  {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
