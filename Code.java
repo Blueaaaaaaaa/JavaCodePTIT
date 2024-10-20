8a350543169a8f547e4a5ede1bff76840c1e53a9
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-10-20 11:57:55 +0700

    push code

--- Code.java
+++ Code.java
@@ -1,17 +1,19 @@
-import java.math.BigInteger;
-import java.util.*;
+package JAVA.CODEPTITJV;
 
-public class J03011
-{
-    public static void main(String[] args)
-    {
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class J03013 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0)
-        {
-            BigInteger a = new BigInteger(sc.nextLine());
-            BigInteger b = new BigInteger(sc.nextLine());
-            System.out.println(a.gcd(b));
+        int t = sc.nextInt();
+        while(t-->0){
+            BigInteger a = sc.nextBigInteger();
+            BigInteger b = sc.nextBigInteger();
+            int max = Math.max(a.toString().length(), b.toString().length());
+            String ans = a.subtract(b).abs().toString();
+            while(ans.length()<max) ans = "0" + ans;
+            System.out.println(ans);
         }
     }
 }
