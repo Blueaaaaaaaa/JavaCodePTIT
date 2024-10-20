617d29f0a039b6154ea2f6854c8548d346e7a2db
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-10-20 11:53:28 +0700

    push code

--- Code.java
+++ Code.java
@@ -1,27 +1,35 @@
 import java.util.*;
 
 public class Code {
-    public static String Reverse(String s) {
-        return new StringBuilder(s).reverse().toString();
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int testcase = sc.nextInt();
+        int n = sc.nextInt();
         sc.nextLine();
-        while (testcase-- > 0) {
-            String s = sc.nextLine();
-            boolean check = true;
-            if (s.charAt(0) != '8' || s.charAt(s.length() - 1) != '8') {
-                check = false;
+        Map<String, Integer> emailCount = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            String[] nameParts = sc.nextLine().trim().toLowerCase().split("\\s+");
+            StringBuilder email = new StringBuilder();
+            
+            // Add the last name (full)
+            email.append(nameParts[nameParts.length - 1]);
+            
+            // Add initials of first and middle names
+            for (int j = 0; j < nameParts.length - 1; j++) {
+                email.append(nameParts[j].charAt(0));
             }
-            int sum = 0;
-            for (int i = 0 ; i < s.length() ; i++) {
-                sum += s.charAt(i) - '0';
+            
+            String baseEmail = email.toString() + "@ptit.edu.vn";
+            
+            // Check if email already exists and add number if necessary
+            if (emailCount.containsKey(baseEmail)) {
+                int count = emailCount.get(baseEmail) + 1;
+                emailCount.put(baseEmail, count);
+                System.out.println(email.toString() + count + "@ptit.edu.vn");
+            } else {
+                emailCount.put(baseEmail, 1);
+                System.out.println(baseEmail);
             }
-            if (sum % 10 != 0) {
-                check = false;
-            }
-            System.out.println(check ? "YES" : "NO");
         }
     }
 }
