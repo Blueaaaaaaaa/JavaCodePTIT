01680459b7a383c29b43381e0b9fd10017677642
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-10-20 11:54:03 +0700

    push code

--- Code.java
+++ Code.java
@@ -1,35 +1,17 @@
+import java.math.BigInteger;
 import java.util.*;
 
-public class Code {
-    public static void main(String[] args) {
+public class J03011
+{
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        Map<String, Integer> emailCount = new HashMap<>();
-        
-        for (int i = 0; i < n; i++) {
-            String[] nameParts = sc.nextLine().trim().toLowerCase().split("\\s+");
-            StringBuilder email = new StringBuilder();
-            
-            // Add the last name (full)
-            email.append(nameParts[nameParts.length - 1]);
-            
-            // Add initials of first and middle names
-            for (int j = 0; j < nameParts.length - 1; j++) {
-                email.append(nameParts[j].charAt(0));
-            }
-            
-            String baseEmail = email.toString() + "@ptit.edu.vn";
-            
-            // Check if email already exists and add number if necessary
-            if (emailCount.containsKey(baseEmail)) {
-                int count = emailCount.get(baseEmail) + 1;
-                emailCount.put(baseEmail, count);
-                System.out.println(email.toString() + count + "@ptit.edu.vn");
-            } else {
-                emailCount.put(baseEmail, 1);
-                System.out.println(baseEmail);
-            }
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0)
+        {
+            BigInteger a = new BigInteger(sc.nextLine());
+            BigInteger b = new BigInteger(sc.nextLine());
+            System.out.println(a.gcd(b));
         }
     }
 }
