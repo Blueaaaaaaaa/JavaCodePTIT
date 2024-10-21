1561701995685e14bb41b2846b356c9f46fe6f84
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-10-21 19:46:25 +0700

    update

--- Code.java
+++ Code.java
@@ -1,29 +1,17 @@
-/*
- * XÂU ĐỐI XỨNG
- * @author TaDuy
- */
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.math.*;
 
-public class J03025 {
-
+public class Code {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            String s = sc.next();
-            int len = s.length();
-            int dem = 0;
-            for (int i = 0; i < len / 2; i++) {
-                if (s.charAt(i) != s.charAt(len - i - 1)) {
-                    dem++;
-                }
+        int testcase = sc.nextInt();
+        while (testcase-- > 0) {
+            int n = sc.nextInt(), h = sc.nextInt();
+            for (int i = 1; i < n; i++) {
+                System.out.printf("%.6f ", Math.sqrt(i / (double) n) * h);
             }
-            if (len % 2 == 1) {
-                System.out.println(dem <= 1 ? "YES" : "NO");
-            } else {
-                System.out.println(dem == 1 ? "YES" : "NO");
-            }
+            System.out.println();
         }
-        sc.close();
     }
 }
