159364f5d88d90a8d9bfb82722a932ad89d1dcb4
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-10-21 22:44:09 +0700

    update

--- Code.java
+++ Code.java
@@ -1,24 +1,38 @@
 import java.util.*;
-import java.util.*;
+
 public class Code {
+    public static boolean Check(String s) {
+        for (char x : s.toCharArray()) {
+            if (x != '0' && x != '1' && x != '8' && x != '9') return false;
+        }
+        return true;
+    }
+
+    public static String cutInHalf(String s) {
+        StringBuilder result = new StringBuilder();
+        for (char x : s.toCharArray()) {
+            if (x == '0' || x == '1') result.append(x);
+            else if (x == '8' || x == '9') result.append('0');
+        }
+        return result.toString().replaceFirst("^0+(?!$)", "");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int testcase = sc.nextInt();
-        for ( int test =  1 ; test <= testcase ; test++){
-            System.out.print("Test "+ test + ": ");
-            int n = sc.nextInt();
-            for ( int i = 2; i * i  <=  n; i++){
-                int count = 0;
-                if (n % i == 0) {
-                    while (n % i == 0) {
-                        count++;
-                        n /= i;
-                    }
-                    System.out.print(i + "("+ count + ") ");
+        sc.nextLine();
+        for (int test = 1; test <= testcase; test++) {
+            String s = sc.nextLine();
+            if (Check(s)) {
+                String result = cutInHalf(s);
+                if (result.isEmpty() || result.equals("0")) {
+                    System.out.println("INVALID");
+                } else {
+                    System.out.println(result);
                 }
+            } else {
+                System.out.println("INVALID");
             }
-            if (n > 1) System.out.print(n + "(1)");
-            System.out.println();
-       }
+        }
     }
 }
