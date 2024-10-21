2dfa2e217c97e2ffb0e04f90d7109f010f683ed3
Blueaaaaaaaa <hailam04.work@gmail.com>
2024-10-21 21:40:38 +0700

    update

--- Code.java
+++ Code.java
@@ -1,17 +1,24 @@
 import java.util.*;
-import java.io.*;
-import java.math.*;
-
+import java.util.*;
 public class Code {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int testcase = sc.nextInt();
-        while (testcase-- > 0) {
-            int n = sc.nextInt(), h = sc.nextInt();
-            for (int i = 1; i < n; i++) {
-                System.out.printf("%.6f ", Math.sqrt(i / (double) n) * h);
+        for ( int test =  1 ; test <= testcase ; test++){
+            System.out.print("Test "+ test + ": ");
+            int n = sc.nextInt();
+            for ( int i = 2; i * i  <=  n; i++){
+                int count = 0;
+                if (n % i == 0) {
+                    while (n % i == 0) {
+                        count++;
+                        n /= i;
+                    }
+                    System.out.print(i + "("+ count + ") ");
+                }
             }
+            if (n > 1) System.out.print(n + "(1)");
             System.out.println();
-        }
+       }
     }
 }
