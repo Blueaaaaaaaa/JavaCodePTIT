29f180b885f261118b68c37f772f1645ef85fc74
Blueaaaaaaaa <Hailam04.work@gmail.com>
2024-11-04 09:12:13 +0700

    Update

---
+++ Java/Main.java
@@ -0,0 +1,7 @@
+package Java;
+
+public class Main {
+   public static void main(String[] args) {
+      System.out.println("Class Java.SinhVien not found. Please ensure the class is defined and available.");
+   }
+}
---
+++ Java/SinhVien.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class SinhVien {
+    public void display() {
+        System.out.println("Display Method");
+    }
+    SinhVien() {
+      System.out.println("Connect To Class Sinh Vien");
+    }
+    public void Hello_World(){
+      System.out.println("Hello, World!");
+    }
+}
