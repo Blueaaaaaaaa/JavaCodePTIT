4e216d0167cfc2d8d71e5cbcc4be52a4c4838a7b
Blueaaaaaaaa <Hailam04.work@gmail.com>
2024-11-19 10:52:06 +0700

    Keep Streak

--- Java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Sinhvien sv = new Sinhvien("Nguyen Van A", "123456", "12/12/2000", 8.5);
-        System.out.println(sv);
-    }
-}
